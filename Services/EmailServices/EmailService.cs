2814fe5befe84b4f038f7d1e4f35dd94550ee212
anupam-gautam <anupam.gautam46@gmail.com>
2022-09-29 11:10:28 +0545

    XML also Working

--- Services/EmailServices/EmailService.cs
+++ Services/EmailServices/EmailService.cs
@@ -2,6 +2,7 @@
 using MailKit.Security;
 using MimeKit;
 using MimeKit.Text;
+using System.Xml;
 
 namespace SimpleEmailApplication.Services.EmailServices
 {
@@ -14,13 +15,28 @@ namespace SimpleEmailApplication.Services.EmailServices
             _config = config;
         }
 
+
+
         public void SendEmail(EmailDto request, string otp)
         {
+            var subjectXmlMessage = ""; 
+           string path = @"F:\Office Work\SimpleEmailApplication\wwwroot\OtpMessageForBody.xml";
+
+           using (FileStream stream = System.IO.File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+           {
+                XmlDocument template = new XmlDocument();
+                template.Load(stream);
+                var Content = template.InnerText;
+                subjectXmlMessage = Content.Replace("{otp}", otp);
+
+            }
+
+
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse(_config.GetSection("SmtpConfiguration").GetSection("EmailUsername").Value));
             email.To.Add(MailboxAddress.Parse(request.To));
-            email.Subject = request.Subject;
-            email.Body = new TextPart(TextFormat.Html) { Text = request.Body + otp };
+            email.Subject = "OTP(One Time Password)";
+            email.Body = new TextPart(TextFormat.Html) { Text = subjectXmlMessage };
 
             using var smtp = new SmtpClient();
             smtp.Connect(_config.GetSection("SmtpConfiguration").GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
