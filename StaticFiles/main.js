f64d79d983683b861aeb8ddac296336b2454bb91
anupam-gautam <anupam.gautam46@gmail.com>
2022-10-10 13:06:15 +0545

    View Made to Put Email and Otp after user receieves it

--- StaticFiles/main.js
+++ StaticFiles/main.js
@@ -1,49 +1,19 @@
 ﻿function doRequest() {
 
-    //fetch("https://localhost:7043/api/Email/SendEmail", {
+    let inputValue = $('#email').val();
 
-    //    // adding method type
-    //    method: "post",
-
-    //    // adding body or contents to send
-    //    body: json.stringify({
-
-    //        To: "foo"
-    //        //body: "bar",
-    //        //userid: 1
-    //    }),
-
-    //    // adding headers to the request
-    //    headers: {
-    //        "content-type": "application/json; charset=utf-8"
-    //    }
-    //})
-
-    //    // converting to json
-    //    .then(response => response.json())
-
-    //    // displaying results to console
-    //    .then(json => console.log(json));
-
-
-
-
-
-        let inputValue = $('#email').val();
-
-        $.ajax({
-            type: "POST",
-            url: "/api/Email/SendEmail",
-            data: JSON.stringify({ "To": inputValue }),
-            contentType: 'application/json;charset=utf-8',
-            //success: alert("DONE"),
-            dataType: "json"
-        });
-
-        
+    $.ajax({
+        type: "POST",
+        url: "/api/Email/SendEmail",
+        data: JSON.stringify({ "To": inputValue }),
+        dataType: "json",
+        contentType: 'application/json;charset=utf-8',
+        success: function (response) {
+            if (response) { // check whether response is received
+                window.location.replace("https://localhost:7043/StaticFiles/OtpVerification.html");
+            }
+        }
+    });
+    
 
 }
-
-
-
-//    location.replace("https://www.w3schools.com")
